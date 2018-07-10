0125fd8dc7ce59e1c133e31293c30b625a51a62c
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-07-10 17:19:34 +0200

    Spielfeld erstellt
    Grundobjekte erstellt. Rasterstellung angefangen.

---
+++ BattleshipAR/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Author: Konstantin Regenhardt*/
+
+public class PlayerController : MonoBehaviour {
+
+    GameObject player;
+    private float boardSizeX;
+    private float boardSizeY;
+    private float tileSize;
+
+    public Transform tile;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boardSizeX = player.GetComponent<Transform>().transform.localScale.x;
+        boardSizeY = player.GetComponent<Transform>().transform.localScale.z;
+
+        for(int i = 0; i < boardSizeX; i++)
+        {
+            for(int j = 0; i < boardSizeY; j++)
+            {
+                Instantiate(tile, new Vector3(i, j, tile.transform.position.z), Quaternion.identity);
+            }
+        }
+    }
+	
+    void Update () {
+		
+	}
+}
